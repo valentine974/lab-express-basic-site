5b09c12ce23fbff43dab27812f01fa1eb183f68a
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2022-12-08 22:21:02 +0000

    connected the pages, basic style to the pages

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static("public"))
+
+
 app.get('/home',(request,response)=> {
     response.sendFile(__dirname + '/views/index.html')
 })
