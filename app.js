7e165c8f10ce0c8e260fe4826be3a1200c41bdb4
valentine974 <valentine.bathany@hotmail.fr>
2022-12-08 21:28:45 +0100

    first iteration, but errors

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/home',(request,response)=> {
+    response.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/about',(request,response)=> {
+    response.sendFile(__dirname + '/views/about.html')})
+
+app.get('/works',(request,response)=> {
+    response.sendFile(__dirname + '/views/works.html')})
+
+app.listen(3000,()=>console.log('server connected'))
